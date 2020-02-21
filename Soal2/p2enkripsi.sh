833a648b06fb85d09cd69a967a94fb1a15429401
adam <garibaldiadam@gmail.com>
2020-02-21 15:55:57 +0700

    rename

--- Soal2/p2enkripsi.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for loop in $(seq 25); do
-	if [ $loop -eq $jam ]
-	  then
-	  bisa=`echo $filename | tr $(printf %${loop}s | tr ' ' '.')\A-Za-z N-ZA-Mn-za-m`
-	fi
-done
-echo $random > $bisa.txt
